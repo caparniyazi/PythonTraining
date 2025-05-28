b44ee5c3ab93f902144e1cbf329671622bd33df9
caparniyazi <capar.niyazi.tr@gmail.com>
2025-05-28 10:47:07 +0300

    Python training, drawing geometric shapes with Turtle toolkit.

---
+++ Turtle Graphics/drawing_geometric_shapes.py
@@ -0,0 +1,23 @@
+import random
+from turtle import Turtle, Screen
+
+# Shapes(triangle, square, pentagon, hexagon, heptagon, octagon, nonagon, decagon).
+FULL_ANGLE = 360
+COLOURS = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray",
+           "SeaGreen"]
+DISTANCE = 100
+tim = Turtle()
+
+
+def draw_shape(number_of_sides):
+    for _ in range(number_of_sides):
+        tim.forward(DISTANCE)
+        tim.right(FULL_ANGLE / number_of_sides)  # internal angle
+
+
+for n in range(3, 11):
+    tim.color(random.choice(COLOURS))
+    draw_shape(n)
+
+screen = Screen()
+screen.exitonclick()
