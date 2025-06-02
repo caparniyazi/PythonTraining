3e2defec646d0717b825a6cd5701f4a67faa6641
caparniyazi <capar.niyazi.tr@gmail.com>
2025-06-02 12:57:08 +0300

    Python training, Snake Game high-score with file I/O implementation.

--- Turtle Graphics/snake/scoreboard.py
+++ Turtle Graphics/snake/scoreboard.py
@@ -4,24 +4,39 @@ from turtle import Turtle
 FONT = ("Courier", 12, "normal")
 
 
+def read_score():
+    with open("data.txt") as f:
+        return int(f.read())
+
+
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.hideturtle()
+        self.high_score = read_score()
         self.color("white")
         self.penup()
         self.goto(0, 270)
+        self.hideturtle()
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}", True, align=CENTER, font=FONT)
+        self.clear()
+        self.write(f"Score: {self.score} High score: {self.high_score}", align=CENTER, font=FONT)
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write(f"GAME OVER", True, align=CENTER, font=FONT)
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            self.write_score()
+
+        # Reset the score
+        self.score = 0
+        self.update_scoreboard()
 
     def inc(self):
         self.score += 1
-        self.clear()
         self.update_scoreboard()
+
+    def write_score(self):
+        with open("data.txt", "w") as f:
+            f.write(f"{self.high_score}")
