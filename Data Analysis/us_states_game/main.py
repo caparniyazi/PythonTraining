240a005e704dee514af38a071f178de449dc3e64
caparniyazi <capar.niyazi.tr@gmail.com>
2025-06-04 11:34:56 +0300

    Python training, US States Game with Pandas.

---
+++ Data Analysis/us_states_game/main.py
@@ -0,0 +1,44 @@
+import turtle
+from turtle import Screen, Turtle
+import pandas
+
+# This is how to print out the coordinates of a loaded map:
+#
+# def get_mouse_click_coor(x, y):
+#     print(x, y)
+#
+# turtle.onscreenclick(get_mouse_click_coor)
+# turtle.mainloop()
+
+screen = Screen()
+screen.title("U.S. States Game")
+image = "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+data = pandas.read_csv("50_states.csv")
+all_states = data.state.to_list()
+guessed_states = []
+
+while len(guessed_states) < len(all_states):
+    answer_state = screen.textinput(title=f"{len(guessed_states)} / {len(all_states)}",
+                                    prompt="What's the state name?").title()
+
+    if answer_state == "Exit":
+        missing_states = []
+
+        for state in all_states:
+            if state not in guessed_states:
+                missing_states.append(state)
+            new_data_frame = pandas.DataFrame(missing_states)
+            new_data_frame.to_csv("states_to_learn.csv")
+        break
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        t = Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(state_data.x.item(), state_data.y.item())
+        t.write(answer_state)
+
+screen.exitonclick()
