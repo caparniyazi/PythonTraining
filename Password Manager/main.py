22e67ce55cbcaf07fa081b505d3c8170600d5d0f
caparniyazi <capar.niyazi.tr@gmail.com>
2025-06-11 17:32:00 +0300

    Python training, Password Manager app improved with JSON data format and search capability.

--- Password Manager/main.py
+++ Password Manager/main.py
@@ -2,6 +2,8 @@ from tkinter import *
 from tkinter import messagebox
 import random
 import pyperclip
+import json
+
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
@@ -17,8 +19,8 @@ def generate_password():
     nr_symbols = random.randint(2, 4)
     nr_numbers = random.randint(2, 4)
     password_letters = [random.choice(letters) for _ in range(nr_letters)]
-    password_symbols = [random.choice(letters) for _ in range(nr_symbols)]
-    password_numbers = [random.choice(letters) for _ in range(nr_numbers)]
+    password_symbols = [random.choice(symbols) for _ in range(nr_symbols)]
+    password_numbers = [random.choice(numbers) for _ in range(nr_numbers)]
     password_list = password_letters + password_symbols + password_numbers
     random.shuffle(password_list)
     password_entry.delete(0, END)
@@ -28,24 +30,68 @@ def generate_password():
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save_password():
-    is_ok = messagebox.askokcancel(title=website_entry.get(),
-                                   message=f"These are the details entered: \nEmail: {email_entry.get()}\n"
-                                           f"Password: {password_entry.get()}\n"
-                                           f"\nIs it OK to save?")
-    if is_ok:
-        if len(website_entry.get()) == 0 or len(password_entry.get()) == 0:
-            messagebox.showinfo(title="Warning", message="All fields are required.")
-        else:
-            with open("data.txt", "a") as data_file:
-                data_file.write(f"{website_entry.get()} | {email_entry.get()} | {password_entry.get()}\n")
+    # is_ok = messagebox.askokcancel(title=website_entry.get(),
+    #                                message=f"These are the details entered: \nEmail: {email_entry.get()}\n"
+    #                                        f"Password: {password_entry.get()}\n"
+    #                                        f"\nIs it OK to save?")
+    # if is_ok:
 
-                clear_fields()
-                website_entry.focus()
+    website = website_entry.get()
+    email = email_entry.get()
+    password = password_entry.get()
+
+    new_data = {
+        website: {
+            "email": email,
+            "password": password
+        }
+    }
+
+    if len(website_entry.get()) == 0 or len(password_entry.get()) == 0:
+        messagebox.showinfo(title="Warning", message="All fields are required.")
+    else:
+        try:
+            with open("data.json", "r") as data_file:
+                # data_file.write(f"{website_entry.get()} | {email_entry.get()} | {password_entry.get()}\n")
+                # Read the old data
+                data = json.load(data_file)
+        except FileNotFoundError:
+            with open("data.json", "w") as data_file:
+                # Dump the new data.
+                json.dump(new_data, data_file, indent=2)
+        else:
+            # Update old data with new data
+            data.update(new_data)
+
+            # Dump the updated data into the file.
+            with open("data.json", "w") as data_file:
+                json.dump(data, data_file, indent=2)
+        finally:
+            clear_fields()
+            website_entry.focus()
 
 
 def clear_fields():
     website_entry.delete(0, END)
     password_entry.delete(0, END)
+
+
+# ---------------------------- FIND PASSWORD ------------------------------- #
+def search():
+    website = website_entry.get()
+
+    try:
+        with open("data.json") as data_file:
+            data = json.load(data_file)
+    except FileNotFoundError:
+        messagebox.showinfo(title="Warning", message="File does not exist.")
+    else:
+        if website in data:
+            email = data[website]["email"]
+            password = data[website]["password"]
+            messagebox.showinfo(title=f"{website}", message=f"Email: {email}\nPassword: {password}")
+        else:
+            messagebox.showinfo(title="Warning", message=f"No details for {website} exists.")
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -68,9 +114,10 @@ password_label = Label(text="Password:")
 password_label.grid(row=3, column=0)
 
 # Entries
-website_entry = Entry(width=35)
-website_entry.grid(row=1, column=1, columnspan=2, sticky="EW")
+website_entry = Entry(width=15)
+website_entry.grid(row=1, column=1, sticky="EW")
 website_entry.focus()
+
 email_entry = Entry(width=35)
 email_entry.grid(row=2, column=1, columnspan=2, sticky="EW")
 email_entry.insert(0, "capar.niyazi.tr@gmail.com")
@@ -78,7 +125,9 @@ password_entry = Entry(width=15)
 password_entry.grid(row=3, column=1, sticky="EW")
 
 # Buttons
-generate_password_button = Button(text="Generate Password", width=20, command=generate_password)
+search_button = Button(text="Search", width=15, command=search)
+search_button.grid(row=1, column=2)
+generate_password_button = Button(text="Generate Password", width=15, command=generate_password)
 generate_password_button.grid(row=3, column=2)
 add_button = Button(text="Add", width=35, command=save_password)
 add_button.grid(row=4, column=1, columnspan=2, sticky="EW")
