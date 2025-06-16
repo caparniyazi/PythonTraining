06c4dfb50f77e4656b64deb3aaa21be4aad79f4f
caparniyazi <capar.niyazi.tr@gmail.com>
2025-06-16 10:19:29 +0300

    Python training, using SMTP to send emails.

--- Smtp/main.py
+++ Smtp/main.py
@@ -1,26 +1,48 @@
-import random
 import datetime as dt
 import smtplib
+import random
+import pandas
+
+PLACEHOLDER = "[NAME]"
+EMAIL = "capar.niyazi.tr@gmail.com"
+PWD = "xxxx xxxx xxxx xxxx"
+SMTP_PROVIDER = "smtp.gmail.com"
 
 
-def send_email(content):
-    my_email = "capar.niyazi.tr@gmail.com"
-    my_password = "xxxx xxxx xxxx xxxx"
-    with smtplib.SMTP("smtp.gmail.com") as connection:
+def send_email(contents, recipient_address):
+    with smtplib.SMTP(SMTP_PROVIDER) as connection:
         connection.starttls()  # Secure the connection
-        connection.login(my_email, password=my_password)
-        connection.sendmail(from_addr=my_email,
-                            to_addrs="niyazi.capar@xyz.com",
-                            msg=f"Subject:Quote of the day.\n\n{content}."
+        connection.login(user=EMAIL, password=PWD)
+        connection.sendmail(from_addr=EMAIL,
+                            to_addrs=recipient_address,
+                            msg=f"Subject:Quote of the day.\n\n{contents}."
                             )
 
 
-# Sending a random quote of the day.
-today = dt.datetime.now()
-the_weekday = today.weekday()
+def check_birthdays():
+    data_frame = pandas.read_csv("birthdays.csv")
+    # print(data_frame)
 
-if the_weekday == 5:
-    with open("quotes.txt") as in_file:
-        quotes = in_file.readlines()
-        quote = random.choice(quotes)
-        send_email(quote)
+    now = dt.datetime.now()
+    today_tuple = (now.month, now.day)
+    birthdays_dic = {(data_row.month, data_row.day): data_row for (index, data_row) in data_frame.iterrows()}
+
+    if today_tuple in birthdays_dic:
+        birthday_person = birthdays_dic[today_tuple]
+        file_path = f"letters/letter_{random.randint(1, 3)}.txt"
+
+        with open(file_path) as letter_file:
+            letter_contents = letter_file.read()
+            contents = letter_contents.replace(PLACEHOLDER, birthday_person["name"])
+            send_email(contents, birthday_person["email"])
+
+
+check_birthdays()
+# my_dict = data_frame.to_dict(orient="records")
+# for item in my_dict:
+#     if item["month"] == now.month and item["day"] == now.day:
+#         name = item["name"]
+#         with open("letters/letter_1.txt") as letter_file:
+#             letter_contents = letter_file.read()
+#             new_letter = letter_contents.replace(PLACEHOLDER, name)
+#             send_email(new_letter)
